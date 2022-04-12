74125e9bd3f5415fb4faf11f3451aecb927e23ed
HrituRaz33 <95464215+HrituRaz33@users.noreply.github.com>
2022-04-13 00:28:33 +0600

    google firebase authentication sign in and sign out added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const { signInWithGoogle } = useFirebase();
     return (
         <div>
             <h1>please login bro!!!</h1>
+            <button onClick={signInWithGoogle}>Google sign in</button>
+            <br />
+            <form>
+                <input type="email" placeholder='Your Email' />
+                <br />
+                <input type="password" name='' id='' placeholder='Your Password' />
+                <br />
+                <input type="submit" value='Login' />
+            </form>
         </div>
     );
 };
